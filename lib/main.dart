404c20034e3de9835c55c22ebef860c186f8deb3
chandan kumar <kumarchandan41u@gmail.com>
2022-06-02 10:58:53 +0530

    crud

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fire_s/screens/EMAIL_AUTH/login_screen.dart';
 import 'package:fire_s/screens/home_screen.dart';
 import 'package:fire_s/screens/phone_Auth/sign_in_with.dart';
@@ -12,6 +15,36 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  // FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // DocumentSnapshot snapshot = await FirebaseFirestore.instance
+  //     .collection("users")
+  //     .doc("VqMlNEqM9t8bu7XQ8jbZ ")
+  //     .get();
+  // log(snapshot.data().toString());
+
+  // for (var doc in snapshot.docs) {
+  //   log(doc.data().toString());
+  // }
+  // // log(snapshot.docs.toString());
+
+//data add
+  // Map<String, dynamic> newUsers = {
+  //   "name": "SlantCode",
+  //   "Email": "slantcode@gmail.com"
+  // };
+  // await _firestore.collection("users").doc("your-id-here").set(newUsers);
+  // log("new saved user ");
+  //update
+  // await _firestore
+  //     .collection("users")
+  //     .doc("your-id-here")
+  //     .update({"email": "test@gmail"});
+  // log("new saved user ");
+  // delete data
+  // await _firestore.collection("users").doc("your-id-here").delete();
+  // log("user Delleted");
+
   runApp(const MyApp());
 }
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 // import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:firebaseseries/screens/email_auth/login_screen.dart';
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fire_s/screens/phone_Auth/sign_in_with.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
@@ -15,6 +18,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  void saveUser() {
+    String name = nameController.text.trim();
+    String email = emailController.text.trim();
+    String ageString = ageController.text.trim();
+    int age = int.parse(ageString);
+    nameController.clear();
+    emailController.clear();
+    ageController.clear();
+    if (name != "" && email != "") {
+      Map<String, dynamic> usersData = {
+        "name": name,
+        'email': email,
+        'age': age
+      };
+      FirebaseFirestore.instance.collection("users").add(usersData);
+      log("user created");
+    } else {
+      log("please fill  all the Fields ");
+    }
+  }
+
   void logOut() async {
     await FirebaseAuth.instance.signOut();
     Navigator.popUntil(context, (route) => route.isFirst);
@@ -35,6 +62,86 @@ class _HomeScreenState extends State<HomeScreen> {
             icon: Icon(Icons.exit_to_app),
           ),
         ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: InputDecoration(hintText: "Name"),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(hintText: "Email Address"),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: ageController,
+                decoration: InputDecoration(hintText: "Age"),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              CupertinoButton(
+                onPressed: () {
+                  saveUser();
+                },
+                child: Text("Save"),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection("users")
+                      .orderBy("age")
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.active) {
+                      if (snapshot.hasData && snapshot.data != null) {
+                        return ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            Map<String, dynamic> userMap =
+                                snapshot.data!.docs[index].data()
+                                    as Map<String, dynamic>;
+                            return ListTile(
+                              title:
+                                  Text(userMap["name"] + "(${userMap['age']})"),
+                              subtitle: Text(userMap['email']),
+                              trailing: IconButton(
+                                onPressed: () async {
+                                  await FirebaseFirestore.instance
+                                      .collection("users")
+                                      .doc(index.toString())
+                                      .delete();
+                                  log("user Delleted");
+                                },
+                                icon: Icon(Icons.delete),
+                              ),
+                            );
+                          },
+                        );
+                      } else {
+                        return Text("No Data !");
+                      }
+                    } else {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
